9ee0cecb180917da3f8722845c873d5ef91efb9f
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-07-04 15:18:33 +0300

    fix: Improve error component UI and enhance error handling logic

--- app/error.tsx
+++ app/error.tsx
@@ -1,13 +1,30 @@
-"use client";
-import React from "react";
+"use client"; // Error boundaries must be Client Components
 
-function Error() {
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
     <div>
-      <h1>Error</h1>
-      <p>Something went wrong.</p>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
     </div>
   );
 }
-
-export default Error;
