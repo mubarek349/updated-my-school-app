f00e3594cd1886f2b222733290b176596e10a26a
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-09-30 17:09:42 +0300

    Refactor middleware to simplify authentication handling; export auth directly from auth.ts and remove redundant session checks and route protections.

--- auth.ts
+++ auth.ts
@@ -118,3 +118,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - login page
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|en/login).*)",
+  ],
+};
