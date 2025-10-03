b5cb382dab7e68a21b20e3a35a67b56ff0be9447
Mubarek Ahmed Mohammed <mubarekahmed349@gmail.com>
2025-10-03 10:08:22 +0300

    bot and middlewated

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
 export { auth as middleware } from "@/auth";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - login page
+     * - student routes (students access via direct links)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|en/login|en/student).*)",
+  ],
+};
